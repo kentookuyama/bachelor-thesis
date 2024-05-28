adjusted data albumentations position to fix normalization issue

core/dataset.py
@@ -1,44 +1,110 @@
-import glob
-import os
-import random
-
-import numpy as np
 import torch
-from core import field
-from skimage.io import imread
 from torch.utils.data import Dataset
+
+from core import field
 
 
 class ColorAugDataset(Dataset):
-    def __init__(
-        self, image_dir, targets_dir, geo_transform, color_transform, common_transform
-    ):
-        self.target_fps = sorted(
-            [fp for fp in glob.glob(os.path.join(targets_dir, "*.png")) if "pre" in fp]
-        )
-
-        self.image_fps = [
-            os.path.join(image_dir, os.path.basename(fp.replace("_target.png", ".png")))
-            for fp in self.target_fps
-        ]
+    def __init__(self, dataset, geo_transform, color_transform, common_transform):
+        self.dataset = dataset
         self.color_transform = color_transform
         self.geo_transform = geo_transform
         self.common_transform = common_transform
 
-    def __getitem__(self, idx):
-        x = imread(self.image_fps[idx])
-        mask = imread(self.target_fps[idx])
-        y = dict()
-        y[field.MASK1] = mask
-        y["image_filename"] = os.path.basename(self.image_fps[idx])
+    # def __init__(
+    #     self, image_dir, targets_dir, geo_transform, color_transform, common_transform
+    # ):
+    #     self.target_fps = sorted(
+    #         [fp for fp in glob.glob(os.path.join(targets_dir, "*.png")) if "pre" in fp]
+    #     )
 
-        # Get initial image and mask shapes for comparison
-        orig_img_shape = x.shape
-        orig_mask_shape = y[field.MASK1].shape
+    #     self.image_fps = [
+    #         os.path.join(image_dir, os.path.basename(fp.replace("_target.png", ".png")))
+    #         for fp in self.target_fps
+    #     ]
+    #     self.color_transform = color_transform
+    #     self.geo_transform = geo_transform
+    #     self.common_transform = common_transform
+
+    def __getitem__(self, idx):
+        x, y = self.dataset[idx]
+
+        if x.shape[2] == 6:
+            # Extract corners and their respective masks from x and y
+            base_corner_np = x[:, :, :3]
+            helper_corner_np = x[:, :, 3:]
+            mask1_np = y[field.MASK1]
+            mask2_np = y[field.VMASK2]
+
+            orig_shapes = {
+                "base_corner": base_corner_np.shape,
+                "helper_corner": helper_corner_np.shape,
+                "mask1": mask1_np.shape,
+                "mask2": mask2_np.shape,
+            }
+
+            # Apply Geo Transforms
+            image_1 = self.geo_transform(
+                **dict(image=base_corner_np, mask=mask1_np)
+            )  # here
+            image_2 = self.geo_transform(**dict(image=helper_corner_np, mask=mask2_np))
+
+            # Verify shapes after geo_transform
+            if (
+                image_1["image"].shape != orig_shapes["base_corner"]
+                or image_1["mask"].shape != orig_shapes["mask1"]
+            ):
+                raise ValueError(
+                    f"Shape mismatch after geo_transform for base_corner or mask1: expected {orig_shapes['base_corner']}, {orig_shapes['mask1']} but got {image_1['image'].shape}, {image_1['mask'].shape}"
+                )
+
+            if (
+                image_2["image"].shape != orig_shapes["helper_corner"]
+                or image_2["mask"].shape != orig_shapes["mask2"]
+            ):
+                raise ValueError(
+                    f"Shape mismatch after geo_transform for helper_corner or mask2: expected {orig_shapes['helper_corner']}, {orig_shapes['mask2']} but got {image_2['image'].shape}, {image_2['mask'].shape}"
+                )
+
+            # Extract transformed images and masks
+            image_1_img = image_1["image"]
+            image_1_mask = image_1["mask"]
+            image_2_img = image_2["image"]
+            image_2_mask = image_2["mask"]
+
+            # x, mask -> tensor (common Transforms)
+            image_1 = self.common_transform(image=image_1_img, mask=image_1_mask)
+            image_2 = self.common_transform(image=image_2_img, mask=image_2_mask)
+
+            # Extract transformed image and mask
+            image_1_img = image_1["image"]
+            image_1_mask = image_1["mask"]
+            image_2_img = image_2["image"]
+            image_2_mask = image_2["mask"]
+
+            # Ensure both images|masks are tensors
+            if (
+                not isinstance(image_1_img, torch.Tensor)
+                or not isinstance(image_1_mask, torch.Tensor)
+                or not isinstance(image_2_img, torch.Tensor)
+                or not isinstance(image_2_mask, torch.Tensor)
+            ):
+                raise TypeError("Expected x and y to be PyTorch tensors")
+
+            # Set data to dataset form required for ChangeMixin
+            org_img = torch.cat([image_1_img, image_2_img], dim=0)
+            y[field.MASK1] = image_1_mask
+            y[field.VMASK2] = image_2_mask
+
+            return org_img, y
 
         blob = self.geo_transform(**dict(image=x, mask=y[field.MASK1]))
         img = blob["image"]
         mask = blob["mask"]
+
+        # Get initial image and mask shapes for comparison
+        orig_img_shape = x.shape
+        orig_mask_shape = y[field.MASK1].shape
 
         # Check image and mask shapes after geometric transformation
         if img.shape != orig_img_shape or mask.shape != orig_mask_shape:
@@ -62,4 +128,4 @@ class ColorAugDataset(Dataset):
         return org_img, y
 
     def __len__(self):
-        return len(self.image_fps)
+        return len(self.dataset)

data/xview2/xview2_dataset.py
@@ -1,72 +1,74 @@
+import glob
+import os
 import random
 
 import cv2
 import numpy as np
 import torch
-from core import field
-from FDA.utils import FDA_source_to_target_np
 from skimage.io import imread
 from torch.utils.data import Dataset
 
+from core import field
+from FDA.utils import FDA_source_to_target_np
+
 
 class PreCachedXview2Building(Dataset):
-    def __init__(self, dataset, transforms=None):
-        # self.target_fps = sorted(
-        #     [fp for fp in glob.glob(os.path.join(targets_dir, "*.png")) if "pre" in fp]
-        # )
+    def __init__(self, image_dir, targets_dir, transforms=None, strategies=None):
+        self.target_fps = sorted(
+            [fp for fp in glob.glob(os.path.join(targets_dir, "*.png")) if "pre" in fp]
+        )
 
-        # self.image_fps = [
-        #     os.path.join(image_dir, os.path.basename(fp.replace("_target.png", ".png")))
-        #     for fp in self.target_fps
-        # ]
-        self.dataset = dataset
+        self.image_fps = [
+            os.path.join(image_dir, os.path.basename(fp.replace("_target.png", ".png")))
+            for fp in self.target_fps
+        ]
         self.transforms = transforms
+        self.strategies = strategies
 
     def __getitem__(self, idx):
-        img, y = self.dataset[idx]
-        mask = y[field.MASK1]
+        x = imread(self.image_fps[idx])
+        mask = imread(self.target_fps[idx])
+        y = dict()
 
-        if self.transforms:
-            blob = self.transforms(**dict(image=img, mask=mask))
-            img = blob["image"]
-            mask = blob["mask"]
+        # if self.transforms:
+        #     blob = self.transforms(**dict(image=img, mask=mask))
+        #     img = blob["image"]
+        #     mask = blob["mask"]
 
-        # y[field.MASK1] = mask
-        # y["image_filename"] = os.path.basename(self.image_fps[idx])
+        y[field.MASK1] = mask
+        y["image_filename"] = os.path.basename(self.image_fps[idx])
 
-        return self.split_image(img, y)
+        return self.split_image(x, y)
 
     def __len__(self):
-        return len(self.dataset)
+        """Returns the length of the dataset"""
+        return len(self.image_fps)
 
     def split_image(self, x, y):
+        """Splits image into 4 corners a,b,c,d
+
+        Dimensions of each corner
+
+        Image: Type: <class 'numpy.ndarray'> Shape: (512, 512, 3)
+
+        Mask: Type: <class 'numpy.ndarray'> Shape: (512, 512)
+        """
         img = x
         mask = y[field.MASK1]
-        c, h, w = img.shape
+        h, w, _ = img.shape
         mid_h, mid_w = h // 2, w // 2
 
         # Corners: a=top-left, b=top-right, c=bottom-left, d=bottom-right
-        corner_a = img[:, :mid_h, :mid_w]
-        corner_b = img[:, :mid_h, mid_w:]
-        corner_c = img[:, mid_h:, :mid_w]
-        corner_d = img[:, mid_h:, mid_w:]
+        corner_a = img[:mid_h, :mid_w, :]
+        corner_b = img[:mid_h, mid_w:, :]
+        corner_c = img[mid_h:, :mid_w, :]
+        corner_d = img[mid_h:, mid_w:, :]
 
         # Masks for each corner
         mask_a = mask[:mid_h, :mid_w]
         mask_b = mask[:mid_h, mid_w:]
         mask_c = mask[mid_h:, :mid_w]
         mask_d = mask[mid_h:, mid_w:]
-
-        # Print shapes after splitting
-        # print(f"Corner a shape: {corner_a.shape}")
-        # print(f"Corner b shape: {corner_b.shape}")
-        # print(f"Corner c shape: {corner_c.shape}")
-        # print(f"Corner d shape: {corner_d.shape}")
-
-        # print(f"Mask a shape: {mask_a.shape}")
-        # print(f"Mask b shape: {mask_b.shape}")
-        # print(f"Mask c shape: {mask_c.shape}")
-        # print(f"Mask d shape: {mask_d.shape}")
 
         corners = {"a": corner_a, "b": corner_b, "c": corner_c, "d": corner_d}
         corner_masks = {"a": mask_a, "b": mask_b, "c": mask_c, "d": mask_d}
@@ -81,32 +83,119 @@ class PreCachedXview2Building(Dataset):
         helper_corner = corners[helper_corner_key]
         helper_mask = corner_masks[helper_corner_key]
 
-        # (
-        #     base_corner,
-        #     base_mask,
-        #     helper_corner,
-        #     helper_mask,
-        # ) = self.semantic_label_copy_paste_pair(
-        #     base_corner, base_mask, helper_corner, helper_mask
-        # )
+        # Select random strategy to apply from activated strategies
+        base_corner, base_mask, helper_corner, helper_mask = self.apply_random_strategy(
+            base_corner, base_mask, helper_corner, helper_mask
+        )
 
-        # Inside the split_image function
-        base_corner = base_corner / 255
-        helper_corner = helper_corner / 255
-        base_mask = base_mask / 255
-        helper_mask = helper_mask / 255
+        # combine the images together.
+        x = np.concatenate([base_corner, helper_corner], axis=2)
 
-        x = torch.cat([base_corner, helper_corner], dim=0)
+        # x = torch.cat([base_corner, helper_corner], dim=2).permute(2, 0, 1)
         y[field.MASK1] = base_mask
         y[field.VMASK2] = helper_mask
 
         return x, y
 
+    def apply_random_strategy(self, base_corner, base_mask, helper_corner, helper_mask):
+        """Applies a random strategy based on the enabled strategies"""
+        # Define available strategies
+        available_strategies = {
+            "random_crop": self.random_crop,
+            "semantic_label_inpainting_pair": self.semantic_label_inpainting_pair,
+            "semantic_label_copy_paste_pair": self.semantic_label_copy_paste_pair,
+        }
+
+        # Filter strategies to only those that are enabled
+        enabled_strategies = [key for key, value in self.strategies.items() if value]
+
+        # Randomly select an enabled strategy
+        selected_strategy = random.choice(enabled_strategies)
+        strategy_method = available_strategies[selected_strategy]
+
+        # Apply the selected strategy
+        return strategy_method(base_corner, base_mask, helper_corner, helper_mask)
+
+    ##### Strategy 1: Random Crop
+    def random_crop(base_corner, base_mask, helper_corner, helper_mask):
+        return base_corner, base_mask, helper_corner, helper_mask
+
+    ##### Strategy 2: Inpainting
+    def semantic_label_inpainting_pair(
+        base_corner, base_mask, helper_corner, helper_mask
+    ):
+        mask = np.array(base_mask.cpu())
+        print(mask.shape)
+        # Find connected components in the mask
+        _, labels, stats, _ = cv2.connectedComponentsWithStats(
+            mask.astype(np.uint8), connectivity=4
+        )
+
+        # Calculate total image area
+        total_area = mask.size
+
+        # Remove the background component (label 0)
+        object_stats = stats[1:]
+
+        # Calculate areas of all objects
+        object_areas = object_stats[:, 4]
+
+        # Calculate the threshold area (30% of total image area)
+        threshold_area = 0.3 * total_area
+
+        # Exclude objects larger than the threshold area
+        eligible_objects = [
+            index for index, area in enumerate(object_areas) if area <= threshold_area
+        ]
+
+        # Randomly select objects to inpaint from eligible objects
+        num_objects_to_remove = np.random.randint(1, len(eligible_objects) + 1)
+        selected_object_indices = np.random.choice(
+            eligible_objects, num_objects_to_remove, replace=False
+        )
+
+        # Create a binary mask for inpainting
+        inpaint_mask = np.zeros_like(mask)
+        for index in selected_object_indices:
+            inpaint_mask[labels == index + 1] = 1
+
+        # Inpainting using OpenCV's inpaint method from TELEA
+        inpainted_corner = cv2.inpaint(
+            base_corner,
+            inpaint_mask.astype(np.uint8),
+            inpaintRadius=25,
+            flags=cv2.INPAINT_TELEA,
+        )
+
+        # Update the mask by removing the selected objects
+        updated_mask = np.where(inpaint_mask == 1, 0, mask)
+        inpainted_corner = torch.from_numpy(inpainted_corner)
+        updated_mask = torch.from_numpy(updated_mask)
+
+        return base_corner, base_mask, inpainted_corner, updated_mask
+
     ##### Strategy 3: copy & paste + Fourier blending
     def semantic_label_copy_paste_pair(
         self, base_corner, base_mask, helper_corner, helper_mask
     ):
+        """
+        Performs the copy and paste strategy with Fourier blending.
+
+        This method first performs a copy and paste of the two images,
+        then uses Fourier blending to smooth out the edges and blend the image.
+
+        Args:
+            base_corner (np.ndarray): Base image corner.
+            base_mask (np.ndarray): Mask for the base image.
+            helper_corner (np.ndarray): Helper image corner.
+            helper_mask (np.ndarray): Mask for the helper image.
+
+        Returns:
+            tuple: Original corner and mask, blended corner and mask.
+        """
+        # Copy the original base corner and mask
         org_corner, org_mask = base_corner.copy(), base_mask.copy()
+
         # Get the indices of non-background pixels in the helper mask
         helper_indices = np.argwhere(helper_mask != 0)
 
@@ -114,7 +203,7 @@ class PreCachedXview2Building(Dataset):
         for idx in helper_indices:
             x, y = idx
 
-            # If the pixel is within the bounds of the base mask, copy the corresponding pixel from the helper corner
+            # Copy pixel from helper to base if within bounds
             if 0 <= x < base_mask.shape[0] and 0 <= y < base_mask.shape[1]:
                 base_corner[x, y, :] = helper_corner[x, y, :]
                 base_mask[x, y] = helper_mask[x, y]
@@ -128,6 +217,21 @@ class PreCachedXview2Building(Dataset):
         return org_corner, org_mask, blended_corner, blended_mask
 
     def fourier_blending(self, source_image, target_image, L=0.01):
+        """
+        Applies Fourier Domain Adaptation (FDA) to blend the source image with the target image.
+
+        This method uses Fourier domain adaptation to transfer the spectral characteristics of the
+        target image to the source image. The blending is controlled by the parameter L, which
+        determines the extent of low-frequency component replacement.
+
+        Args:
+            source_image (numpy.ndarray): The source image, expected in (H, W, C) format.
+            target_image (numpy.ndarray): The target image, expected in (H, W, C) format.
+            L (float, optional): The low-frequency component scaling factor. Default is 0.01.
+
+        Returns:
+            numpy.ndarray: The blended image in (H, W, C) format.
+        """
         # Ensure source and target images are in the same format
         source_image = source_image.transpose((2, 0, 1))
         target_image = target_image.transpose((2, 0, 1))

data/xview2/xview2_loader.py
@@ -11,9 +11,10 @@ from albumentations import (
     RandomRotate90,
     VerticalFlip,
 )
+from torch.utils.data import ConcatDataset, SequentialSampler
+
 from core.dataset import ColorAugDataset
 from data.xview2.xview2_dataset import PreCachedXview2Building
-from torch.utils.data import ConcatDataset, SequentialSampler
 
 
 def seed_worker(worker_id):
@@ -34,37 +35,37 @@ class PreCachedXview2BuildingLoader(er.ERDataLoader):
         else:
             transform = self.config.common_transforms
 
-        ## 1. Perform data augmentations { horizontal and vertical flip, rotation of 90·k (k = 1, 2, 3) degree, and scale jitter }
-
+        ## 1. self-pair generated (two 512x512 images)
         if isinstance(self.config.image_dir, (tuple, list)):
             dataset_list = []
             for im_dir, target_dir in zip(
                 self.config.image_dir, self.config.target_dir
             ):
                 dataset_list.append(
-                    ColorAugDataset(
-                        im_dir,
-                        target_dir,
-                        geo_transform=self.config.geo_transforms,
-                        color_transform=self.config.color_transforms,
-                        common_transform=self.config.common_transforms,
+                    PreCachedXview2Building(
+                        im_dir, target_dir, transform, self.config.strategies
                     )
                 )
-                dataset = ConcatDataset(dataset_list)
+
+            dataset = ConcatDataset(dataset_list)
+
         else:
+            dataset = PreCachedXview2Building(
+                self.config.image_dir, self.config.target_dir, transform
+            )
+
+        ## 2.1 Data Augmentatioons (Geometric Transforms, RandomDiscreteScale)
+        ## 2.2 Common Transforms (Normalization)
+        ## 2.3 Convert to Tensor
+        if self.config.training:
             dataset = ColorAugDataset(
-                self.config.image_dir,
-                self.config.target_dir,
+                dataset,
                 geo_transform=self.config.geo_transforms,
                 color_transform=self.config.color_transforms,
                 common_transform=self.config.common_transforms,
             )
 
         print(f"Dataloader length : {len(dataset)}")
-
-        ## 2. Perform Random Cropping into 512x512 pixels for xview2 pre-disaster data
-        if self.config.training:
-            dataset = PreCachedXview2Building(dataset, transform)
 
         print(f"Dataloader length after xview2 : {len(dataset)}")
 